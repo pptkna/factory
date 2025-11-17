e9b1902e862ab6b6ca2b9abb394f10cd65dd7c46
Konstantin Petrushin <lowskillkn@gmail.com>
2025-11-17 08:37:26 +0300

    remove useless code

--- inventory/internal/api/part/v1/list_parts.go
+++ inventory/internal/api/part/v1/list_parts.go
@@ -31,31 +31,3 @@ func (a *api) ListParts(ctx context.Context, req *inventoryV1.ListPartsRequest) 
 		Parts: partsProto,
 	}, nil
 }
-
-// func (s *inventoryService) ListParts(_ context.Context, req *inventory_v1.ListPartsRequest) (*inventory_v1.ListPartsResponse, error) {
-// 	s.mu.RLock()
-
-// 	parts := make([]*inventory_v1.Part, 0, len(s.parts))
-
-// 	for _, p := range s.parts {
-// 		parts = append(parts, p)
-// 	}
-
-// 	s.mu.RUnlock()
-
-// 	filters := req.GetFilter()
-// 	if len(filters.Uuids) == 0 && len(filters.Categories) == 0 && len(filters.ManufacturerCountries) == 0 && len(filters.Names) == 0 && len(filters.Tags) == 0 {
-// 		return &inventory_v1.ListPartsResponse{Parts: parts}, nil
-// 	}
-
-// 	filteredParts := make([]*inventory_v1.Part, 0, len(parts))
-// 	for _, p := range parts {
-// 		if matchesFilter(p, filters) {
-// 			filteredParts = append(filteredParts, p)
-// 		}
-// 	}
-
-// 	return &inventory_v1.ListPartsResponse{
-// 		Parts: filteredParts,
-// 	}, nil
-// }
